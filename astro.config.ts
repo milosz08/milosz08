eec1dffd11032a2d774e1d559835191554671fca
Milosz08 <gilgamilosz451@gmail.com>
2024-02-15 13:25:43 +0100

    fix CI/CD pipeline

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,15 @@
  * Copyright (c) 2024 by MILOSZ GILGA <https://miloszgilga.pl>
  * For check application license, check LICENSE file.
  */
-import mdx from "@astrojs/mdx";
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
+
+let BASE_URL = 'https://miloszgilga.pl';
+if (import.meta.env.DEV) {
+  BASE_URL = 'http://localhost:3254';
+}
 
 export default defineConfig({
   integrations: [mdx()],
+  site: BASE_URL,
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="../.astro/types.d.ts" />
 /*
  * Copyright (c) 2024 by MILOSZ GILGA <https://miloszgilga.pl>
  * For check application license, check LICENSE file.
