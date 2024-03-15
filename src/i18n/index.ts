7998546e520c8f461ee58d13e9f224832f2046f4
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-03-15 03:46:23 +0100

    create basic structure for mdx projects and home page

---
+++ src/i18n/index.ts
@@ -0,0 +1,36 @@
+import { ui } from '@/i18n/ui-translations';
+
+export const AVAILABLE_LANGS: TLanguage[] = ['pl', 'en'];
+export const DEFAULT_LANG = 'en';
+
+export type TLanguage = 'pl' | 'en';
+
+export function getLangFromUrl(url: URL): TLanguage {
+  const [, lang] = url.pathname.split('/');
+  if (AVAILABLE_LANGS.includes(lang as TLanguage)) {
+    return lang as TLanguage;
+  }
+  return DEFAULT_LANG;
+}
+
+export function toggleLangHref(url: string, lang: TLanguage): string {
+  const urlWithoutLang = url.replace(/\/(en|pl)/i, '');
+  return `/${lang}${urlWithoutLang}`;
+}
+
+export function isLang(url: string, lang: TLanguage): boolean {
+  return lang === url.substring(1, 3);
+}
+
+export function useTranslations(lang: TLanguage) {
+  return function t(key: keyof (typeof ui)[typeof DEFAULT_LANG]) {
+    return ui[lang][key] || ui[DEFAULT_LANG][key];
+  };
+}
+
+export function i18nHref(originalUrl: string, lang: TLanguage): string {
+  if (lang === DEFAULT_LANG) {
+    return originalUrl;
+  }
+  return `/${lang}${originalUrl}`;
+}
---
+++ src/i18n/ui-translations.ts
@@ -0,0 +1,14 @@
+export const ui = {
+  en: {
+    'about.projects.impl': 'My projects',
+    'about.projects.rd': 'My R&D projects',
+    'about.projects.part': 'Co-developed projects',
+    'project.return': 'Return to main page',
+  },
+  pl: {
+    'about.projects.impl': 'Moje projekty',
+    'about.projects.rd': 'Moje projekty R&D',
+    'about.projects.part': 'Projekty współtworzone',
+    'project.return': 'Powrót do strony głównej',
+  },
+} as const;
