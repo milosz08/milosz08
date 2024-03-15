844ddcb3a086ba613517f70bb7c3c5382dd8691e
Milosz08 <gilgamilosz451@gmail.com>
2024-03-15 21:57:33 +0100

    minor changes and fix multi-language header

--- src/i18n/ui-translations.ts
+++ src/i18n/ui-translations.ts
@@ -1,11 +1,13 @@
 export const ui = {
   en: {
+    'about.header': 'Miłosz Gilga, BEng',
     'about.projects.impl': 'My projects',
     'about.projects.rd': 'My R&D projects',
     'about.projects.part': 'Co-developed projects',
     'project.return': 'Return to main page',
   },
   pl: {
+    'about.header': 'inż. Miłosz Gilga',
     'about.projects.impl': 'Moje projekty',
     'about.projects.rd': 'Moje projekty R&D',
     'about.projects.part': 'Projekty współtworzone',
