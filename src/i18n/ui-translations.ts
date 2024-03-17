3f7a34f6034d631dc554a23b0339a6ba23bfade7
Milosz08 <gilgamilosz451@gmail.com>
2024-03-17 16:44:40 +0100

    add seo meta tags

--- src/i18n/ui-translations.ts
+++ src/i18n/ui-translations.ts
@@ -1,5 +1,7 @@
 export const ui = {
   en: {
+    'meta.description':
+      'Master of science student at Silesian University of Technology (faculty of Automatic Control, Electronics and Computer Science, spec: Internet and network technologies).',
     'header.translate': 'Zmień na polski',
     'about.header': 'Miłosz Gilga, BEng',
     'projects.header': 'Miłosz Gilga, BEng - projects',
@@ -9,6 +11,8 @@ export const ui = {
     'project.return': 'Return to main page',
   },
   pl: {
+    'meta.description':
+      'Student studiów magisterskich na Politechnice Śląskiej (wydział Automatyki, Elektroniki i Informatyki, specjalność: Internet i technologie sieciowe).',
     'header.translate': 'Change to English',
     'about.header': 'inż. Miłosz Gilga',
     'projects.header': 'inż. Miłosz Gilga - projekty',
