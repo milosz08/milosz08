1a488eaf65eed0ddb084df3bb03ede22e76e51da
Milosz08 <gilgamilosz451@gmail.com>
2024-03-17 03:15:17 +0100

    minor style changes and add i18n content

--- src/i18n/ui-translations.ts
+++ src/i18n/ui-translations.ts
@@ -1,16 +1,20 @@
 export const ui = {
   en: {
+    'header.translate': 'Zmień na polski',
     'about.header': 'Miłosz Gilga, BEng',
-    'about.projects.impl': 'My projects',
-    'about.projects.rd': 'My R&D projects',
-    'about.projects.part': 'Co-developed projects',
+    'projects.header': 'Miłosz Gilga, BEng - projects',
+    'projects.impl': 'My projects',
+    'projects.rd': 'My R&D projects',
+    'projects.part': 'Co-developed projects',
     'project.return': 'Return to main page',
   },
   pl: {
+    'header.translate': 'Change to English',
     'about.header': 'inż. Miłosz Gilga',
-    'about.projects.impl': 'Moje projekty',
-    'about.projects.rd': 'Moje projekty R&D',
-    'about.projects.part': 'Projekty współtworzone',
+    'projects.header': 'inż. Miłosz Gilga - projekty',
+    'projects.impl': 'Moje projekty',
+    'projects.rd': 'Moje projekty R&D',
+    'projects.part': 'Projekty współtworzone',
     'project.return': 'Powrót do strony głównej',
   },
 } as const;
