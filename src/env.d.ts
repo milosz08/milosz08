d1cad4c2294c341971d0487b407333614ac3e678
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-08-10 04:20:02 +0200

    remove unused project files from previous SSG engine

--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2024 by MILOSZ GILGA <https://miloszgilga.pl>
- * For check application license, check LICENSE file.
- */
-
-/// <reference types="astro/client" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
